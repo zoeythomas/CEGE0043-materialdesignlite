fc2aa5b76af98d7e1bbf5c93dd857f123e84a511
zoeythomas <ucfscde@ucl.ac.uk>
2019-02-26 16:28:46 -0500

    Adding start up

--- ucestz1/www/js/startup.js
+++ ucestz1/www/js/startup.js
@@ -1,3 +1,9 @@
+function startup() {
+document.addEventListener('DOMContentLoaded', function() {
+trackAndCircle ();
+}, false);
+}
+
 function trackAndCircle(){
 trackLocation()
 addPointLinePoly()
