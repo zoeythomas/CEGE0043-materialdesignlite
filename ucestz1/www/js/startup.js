698ea88aa867b3562bb961ff69eb3512d7a6ffcc
zoeythomas <zoeythomas@163.com>
2019-03-06 09:56:57 -0500

    modify index ajax

--- ucestz1/www/js/startup.js
+++ ucestz1/www/js/startup.js
@@ -1,17 +1,19 @@
 function startup() {
 document.addEventListener('DOMContentLoaded', function() {
-trackAndCircle ();
+//trackAndCircle (); #如果要得到portnumber就别在startup中call trackAndCircle
 getPort();
 }, false);
 }
 
 function trackAndCircle(){
 trackLocation()
-addPointLinePoly()
-getEarthquakes()
+//addPointLinePoly()
+//getEarthquakes()
 loadW3HTML()
 }
+//必须要将trackAndCircle套在getport之中不然无法得到portnumber
 
 function loadW3HTML() {
 w3.includeHTML();
 }
+//startup时把表格导入index里面
