d6e5deb5a2c66626bc4b7227382ed0036d225cf8
zoeythomas <zoeythomas@163.com>
2019-03-09 10:30:31 -0500

    modify load the closest point form

--- ucestz1/www/js/popup.js
+++ ucestz1/www/js/popup.js
@@ -1,50 +1,54 @@
 var xhrFormData;
 function startFormDataLoad() {
-	xhrFormData = new XMLHttpRequest();
-	var url = "http://developer.cege.ucl.ac.uk:"+httpPortNumber;
-	url = url + "/getGeoJSON/formdata/geom/"+httpPortNumber;
-	xhrFormData.open("GET", url, true);
-	xhrFormData.onreadystatechange = formDataResponse;
-	xhrFormData.send();
+xhrFormData = new XMLHttpRequest();
+var url = "http://developer.cege.ucl.ac.uk:"+httpPortNumber;
+url = url + "/getGeoJSON/formdata/geom/"+httpPortNumber;
+xhrFormData.open("GET", url, true);
+xhrFormData.onreadystatechange = formDataResponse;
+xhrFormData.send();
 }
 function formDataResponse(){
-	if (xhrFormData.readyState == 4) {
-		// once the data is ready, process the data
-		var formData = xhrFormData.responseText;
-		loadFormData(formData);
-	}
+if (xhrFormData.readyState == 4) {
+// once the data is ready, process the data
+var formData = xhrFormData.responseText;
+loadFormData(formData);
+}
 }
 // keep the layer global so that we can automatically pop up a
 // pop-up menu on a point if necessary
+
 // we can also use this to determine distance for the proximity alert
 var formLayer;
 function loadFormData(formData) {
-	// convert the text received from the server to JSON
-	var formJSON = JSON.parse(formData);
-	// load the geoJSON layer
-	formLayer = L.geoJson(formJSON,
-	{
-		// use point to layer to create the points
-		pointToLayer: function (feature, latlng)
-		{
-			// in this case, we build an HTML DIV string
-			// using the values in the data
-			var htmlString = "<DIV id='popup'"+ feature.properties.id + "><h2>" + feature.properties.name + "</h2><br>";
-			htmlString = htmlString + "<h3>"+feature.properties.surname + "</h3><br>";
-			htmlString = htmlString + "<input type='radio' name='answer' id='"+feature.properties.id+"_1'/>"+feature.properties.module+"<br>";
-			htmlString = htmlString + "<input type='radio' name='answer' id='"+feature.properties.id+"_2'/>"+feature.properties.language+"<br>";
-			htmlString = htmlString + "<input type='radio' name='answer' id='"+feature.properties.id+"_3'/>"+feature.properties.lecturetime+"<br>";
-			htmlString = htmlString + "<input type='radio' name='answer' id='"+feature.properties.id+"_4'/>"+feature.properties.port_id+"<br>";
-			htmlString = htmlString + "<button onclick='checkAnswer(" + feature.properties.id + ");return false;'>Submit Answer</button>";
-			// now include a hidden element with the answer
-			// in this case the answer is alwasy the first choice
-			// for the assignment this will of course vary - you can use feature.properties.correct_answer
-			htmlString = htmlString + "<div id=answer" + feature.properties.id + " hidden>1</div>";
-			htmlString = htmlString + "</div>";
-			return L.marker(latlng).bindPopup(htmlString);
-		},
-	}).addTo(mymap);
-	mymap.fitBounds(formLayer.getBounds());
+// convert the text received from the server to JSON
+var formJSON = JSON.parse(formData);
+// load the geoJSON layer
+formLayer = L.geoJson(formJSON,
+{
+// use point to layer to create the points
+pointToLayer: function (feature, latlng)
+{
+// in this case, we build an HTML DIV string
+// using the values in the data
+var htmlString = "<DIV id='popup'"+ feature.properties.id + "><h2>" +
+feature.properties.name + "</h2><br>";
+htmlString = htmlString + "<h3>"+feature.properties.surname +
+"</h3><br>";
+htmlString = htmlString + "<input type='radio' name='answer' id='"+feature.properties.id+"_1'/>"+feature.properties.module+"<br>";
+htmlString = htmlString + "<input type='radio' name='answer' id='"+feature.properties.id+"_2'/>"+feature.properties.language+"<br>";
+htmlString = htmlString + "<input type='radio' name='answer' id='"+feature.properties.id+"_3'/>"+feature.properties.lecturetime+"<br>";
+htmlString = htmlString + "<input type='radio' name='answer' id='"+feature.properties.id+"_4'/>"+feature.properties.port_id+"<br>";
+htmlString = htmlString + "<button onclick='checkAnswer(" +
+feature.properties.id + ");return false;'>Submit Answer</button>";
+// now include a hidden element with the answer
+// in this case the answer is alwasy the first choice
+// for the assignment this will of course vary - you can use feature.properties.correct_answer
+htmlString = htmlString + "<div id=answer" + feature.properties.id +" hidden>1</div>";
+htmlString = htmlString + "</div>";
+return L.marker(latlng).bindPopup(htmlString);
+},
+}).addTo(mymap);
+mymap.fitBounds(formLayer.getBounds());
 }
 
 function checkAnswer(questionID) {
@@ -60,7 +64,8 @@ answerSelected = i;
 }
 if ((document.getElementById(questionID+"_"+i).checked) && (i ==
 answer)) {
-	alert ("Well done");
+
+alert ("Well done");
 correctAnswer = true;
 }
 }
